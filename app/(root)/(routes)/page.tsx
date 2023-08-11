a8787c14512f22c26b285645d01b9d30f6c85078
Pradeep Yadav <pradeepyadav101010@gmail.com>
2023-08-11 06:37:06 +0530

    added navbar, searchfunctinality and initial setup

---
+++ app/(root)/(routes)/page.tsx
@@ -0,0 +1,11 @@
+import SearchInput from "@/components/search-input";
+import { UserButton } from "@clerk/nextjs";
+
+
+export default function Home() {
+  return (
+  <div className="h-full p-4 space-y-2">
+    <SearchInput/>
+  </div>
+  )
+}
