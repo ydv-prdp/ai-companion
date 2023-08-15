53c8f7ba59b7e32a671fae89ae94066e33e66deb
Pradeep Yadav <pradeepyadav101010@gmail.com>
2023-08-15 22:45:18 +0530

    chat ui

--- app/(chat)/(routes)/chat/[chatId]/companions/client.tsx
+++ app/(chat)/(routes)/chat/[chatId]/companions/client.tsx
@@ -1,7 +1,13 @@
 'use client';
 
 import ChatHeader from "@/components/chat-header";
+import {useCompletion}from "ai/react";
 import { Companion, Message } from "@prisma/client";
+import { useRouter } from "next/navigation";
+import { FormEvent, useState } from "react";
+import ChatForm from "@/components/chat-form";
+import ChatMessages from "@/components/chat-messages";
+import { ChatMessageeProps } from "@/components/chat-message";
 
 interface ChatClientProps{
     companion: Companion & {
@@ -13,9 +19,46 @@ interface ChatClientProps{
 }
 
 const ChatClient = ({companion}:ChatClientProps) => {
+  const router = useRouter();
+  const [messages, setMessages] = useState<any[]>(companion.messages);
+  const {input, isLoading, handleInputChange, handleSubmit, setInput} = useCompletion({
+    api:`/api/chat/${companion.id}`,
+    onFinish(prompt, completion) {
+        const systemMessage:ChatMessageeProps = {
+          role:"system",
+          content:completion
+        }
+        setMessages((current)=>[...current, systemMessage]);
+        setInput(""); 
+        router.refresh();
+    },
+  });
+
+  const onSubmit = (e:FormEvent<HTMLFormElement>)=>{
+    const userMessage:ChatMessageeProps = {
+      role:"user",
+      content:input,
+    }
+    setMessages((current)=>[...current, userMessage]);
+    handleSubmit(e);
+
+  }
+
+
   return (
     <div className="flex flex-col h-full p-4 space-y-2">
         <ChatHeader companion={companion}/>
+        <ChatMessages
+          companion={companion}
+          isLoading={isLoading}
+          messages={messages}
+        />
+        <ChatForm
+          isLoading={isLoading}
+          input={input}
+          handleInputChange={handleInputChange}
+          onSubmit={onSubmit}
+        />
     </div>
   )
 }
---
+++ components/chat-messages.tsx
@@ -0,0 +1,56 @@
+import { Companion, Message } from "@prisma/client"
+import ChatMessage, { ChatMessageeProps } from "./chat-message";
+import { ElementRef, useEffect, useRef, useState } from "react";
+
+interface ChatMessagesProps{
+    companion:Companion;
+    isLoading:boolean;
+    messages:ChatMessageeProps[];
+}
+
+const ChatMessages = ({companion, isLoading, messages=[]}:ChatMessagesProps) => {
+  const scrollRef = useRef<ElementRef<"div">>(null);
+  const [fakeLoading, setFakeLoading] = useState(messages.length === 0 ? true: false);
+  useEffect(()=>{
+    const timer = setTimeout(()=>{
+      setFakeLoading(false)
+    },1000)
+
+  return ()=>{
+    clearTimeout(timer);
+  }
+  },[messages.length])
+  useEffect(()=>{
+    scrollRef?.current?.scrollIntoView({behavior:"smooth"})
+  },[])
+
+  return (
+    <div className="flex-1 overflow-y-auto pr-4">
+        <ChatMessage
+            isLoading = {fakeLoading}
+            src={companion.src}
+            role="system"
+            content={`Hello, I am ${companion.name}, ${companion.description}`}
+        />
+        {messages.map((message)=>(
+          <ChatMessage
+            key={message.content}
+            role={message.role}
+            content={message.content}
+            src={message.src}          
+          />
+        ))}
+        {isLoading && (
+          <ChatMessage
+            role="system"
+            src={companion.src}
+            isLoading
+          />
+        )}
+
+       <div ref={scrollRef}/>
+    </div>
+  )
+}
+
+export default ChatMessages
