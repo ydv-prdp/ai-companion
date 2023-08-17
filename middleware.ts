01ba2d55711c73d82371f85ef2b8550aea737570
Pradeep Yadav <pradeepyadav101010@gmail.com>
2023-08-17 22:56:07 +0530

    middleware chat route modified

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,7 @@ import { authMiddleware } from "@clerk/nextjs";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/nextjs/middleware for more information about configuring your middleware
 
-export default authMiddleware({ publicRoutes: ['/', '/api/webhook/clerk', '/chat'], ignoredRoutes: ['/api/webhook/clerk','/chat'], });
+export default authMiddleware({ publicRoutes: ['/', '/api/webhook/clerk', '/chat/40503c02-038b-40ad-8fa8-843a24052abb','/chat/8ad02d54-fc3e-4bd7-96ab-a91e4cc8a1b3'], ignoredRoutes: ['/api/webhook/clerk','/chat/40503c02-038b-40ad-8fa8-843a24052abb', '/chat/8ad02d54-fc3e-4bd7-96ab-a91e4cc8a1b3'], });
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
